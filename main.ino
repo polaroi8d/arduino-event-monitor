c74abcecef92922c7ab50a2e259f70cb44c2d165
Levente Orban <orbanl@inf.u-szeged.hu>
2017-11-12 14:36:39 +0100

    Change the code structure to the ligthning sensor.

--- main.ino
+++ main.ino
@@ -12,6 +12,7 @@ SoftwareSerial BLESerial(TX_PIN, RX_PIN); // TX, RX
 int phoVal;
 int i_data = 0;
 int tmp_address;
+int read_tmp = 0;
 
 // BLE communication
 char recieve_buff;
@@ -19,6 +20,7 @@ char recieve_buff;
 // EEPROM variables
 byte high;
 byte low;
+byte readEEPROM;
 
 void setup() {
   Serial.begin(9600);
@@ -29,7 +31,7 @@ void setup() {
 
   // Write a 0 to all 1024 bytes of the EEPROM
   // 1024 bytes in the ATmega328 EEPROM
-  for (int z=0; z<1024; z++) {
+  for (int z = 0; z < 1024; z++) {
     EEPROM.write(z, 0);
     digitalWrite(13, HIGH); // Turn led on until formating the memory
   }
@@ -39,90 +41,81 @@ void setup() {
 void loop() {
   if (BLESerial.available()) {
     recieve_buff = BLESerial.read();
+    Serial.print(recieve_buff);
     switch (recieve_buff) {
-      case 'h':
+      case 'h': // h 104
         Serial.println("Get the help message for usage.");
         BLESerial.println("*** Manual for commands ***");
-        BLESerial.println("ll - get the actual light value");
-        BLESerial.println("-lc - list of the config values");
-        BLESerial.println("-lo - set [ON] light limit");
-        BLESerial.println("-lq - set [OFF] light limit");
-        BLESerial.println("-lr - read the values from memory");
-        BLESerial.println("-ls - Start sampling the data");
+        BLESerial.println("l - get the actual light value");
+        BLESerial.println("c - list of the config values");
+        BLESerial.println("o - set [ON] light limit");
+        BLESerial.println("q - set [OFF] light limit");
+        BLESerial.println("r - read the values from memory");
+        BLESerial.println("s - Start sampling the data");
         break;
-      case 'l':
-        if (BLESerial.available()) {
-          recieve_buff = BLESerial.read();
-          switch (recieve_buff) {
-            case 'c':
-              BLESerial.println("Light module configuration");
-              BLESerial.print("ON limit: ");
-              BLESerial.println(EEPROMReadInt(0));
-              BLESerial.print("OFF limit: ");
-              BLESerial.println(EEPROMReadInt(2));
-              break;
-            case 'o':
-              EEPROMWriteInt(0, phoVal);
-              BLESerial.print("Light ON config saved: ");
-              BLESerial.println(phoVal);
-              break;
-            case 'q':
-              EEPROMWriteInt(2, phoVal);
-              BLESerial.print("Light OFF config saved: ");
-              BLESerial.println(phoVal);
-              break;
-            case 'l':
-              BLESerial.print("Read the light value: ");
-              BLESerial.println(phoVal);
-              break;
-            case 't':
-              photo_tlimit = BLESerial.read();
-              BLESerial.print("Photoresistor threshold limit: ");
-              BLESerial.println(photo_tlimit);
-            case 'r':
-              byte tmp_data;
-              while (tmp_data != 0) {
-                int i = 0;
-                tmp_data = EEPROM.read(i);
-                Serial.println(tmp_data);
-                digitalWrite(13, HIGH); // Turn led on until formating the memory
-                i++;
-              } 
-              digitalWrite(13, LOW);
-              break;
-            case 's':
-              BLESerial.print("Start the sampling of the photoresistor values");
-              BLESerial.print("Use @ to stop it");
-              byte STATUS;
-              while(BLESerial.read() != '@') {
-                int tmp_address = 4;
-                delay(1500);
-                phoVal = analogRead(PHOTORESIS_PIN);
-                int tmp_min_pho = EEPROMReadInt(0);
-                int tmp_max_pho = EEPROMReadInt(2);
-                if (phoVal > (tmp_min_pho+100)) {
-                  if (STATUS == 1){
-                    BLESerial.println("A lampa allapota lekapcsolt lett.");
-                  }
-                  Serial.println("Lightning off!");
-                  EEPROM.write(tmp_address++, 'L');
-                  STATUS = 0;
-                } else if (phoVal < (tmp_max_pho-100)) {
-                  if (STATUS == 0){
-                    BLESerial.println("A lampa allapota felkapcsolt lett.");
-                  }
-                  Serial.println("Lightning on!");
-                  EEPROM.write(tmp_address++, 'F');
-                  STATUS = 1;
-                } else {
-                  Serial.println(phoVal);
-                }
-              }
-              break;
-            default:
-              BLESerial.println("Command not found. Use 'h' for command list");
+      case 'l': // l 108
+        phoVal = analogRead(PHOTORESIS_PIN);
+        BLESerial.print("Read the light value: ");
+        BLESerial.println(phoVal);
+        break;
+      case 'c': // c 99
+        BLESerial.println("Light module configuration");
+        BLESerial.print("ON limit: ");
+        BLESerial.println(EEPROMReadInt(0));
+        BLESerial.print("OFF limit: ");
+        BLESerial.println(EEPROMReadInt(2));
+        break;
+      case 'o': // o 111
+        phoVal = analogRead(PHOTORESIS_PIN);
+        EEPROMWriteInt(0, phoVal);
+        BLESerial.print("Light ON config saved: ");
+        BLESerial.println(phoVal);
+        break;
+      case 'q': // q 113
+        phoVal = analogRead(PHOTORESIS_PIN);
+        EEPROMWriteInt(2, phoVal);
+        BLESerial.print("Light OFF config saved: ");
+        BLESerial.println(phoVal);
+        break;
+      case 'r': // r 114
+        Serial.println("Read the memory data:");
+        while (read_tmp != 1023) {
+          readEEPROM = EEPROM.read(read_tmp);
+          Serial.print(read_tmp);
+          Serial.print(". byte is ");
+          Serial.println(readEEPROM);
+          digitalWrite(13, HIGH); // Turn led on until formating the memory
+          read_tmp++;
+        }
+        digitalWrite(13, LOW);
+        break;
+      case 's': // s 115
+        BLESerial.print("Start the sampling of the photoresistor values");
+        BLESerial.print("Use @ to stop it");
+        tmp_address = 4;
+        byte STATUS;
+        while (BLESerial.read() != '@') {
+          delay(1500);
+          phoVal = analogRead(PHOTORESIS_PIN);
+          if (phoVal > (EEPROMReadInt(0) + 100)) {
+            if (STATUS == 1) {
+              BLESerial.println("The lamp is turn off.");
+            }
+            Serial.println("Lightning off!");
+            EEPROM.write(tmp_address++, STATUS);
+            STATUS = 0;
+          } else if (phoVal < (EEPROMReadInt(2) - 100)) {
+            if (STATUS == 0) {
+              BLESerial.println("The lamp is turn on.");
+            }
+            Serial.println("Lightning on!");
+            EEPROM.write(tmp_address++, STATUS);
+            STATUS = 1;
+          } else {
+            Serial.println(phoVal);
           }
         }
+        BLESerial.println("Stoped the sampling.");
         break;
       default:
         BLESerial.println("Command not found. Check out the available commands with 'h'.");
@@ -136,15 +129,15 @@ void EEPROMWriteInt(int address, int value)
 {
   byte two = (value & 0xFF);
   byte one = ((value >> 8) & 0xFF);
-  
+
   EEPROM.update(address, two);
   EEPROM.update(address + 1, one);
 }
- 
+
 int EEPROMReadInt(int address)
 {
   long two = EEPROM.read(address);
   long one = EEPROM.read(address + 1);
- 
+
   return ((two << 0) & 0xFFFFFF) + ((one << 8) & 0xFFFFFFFF);
 }
