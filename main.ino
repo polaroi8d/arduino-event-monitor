79c53e60e4850c53fc19fd3ec373903a6d20e975
Levente Orban <orbanl@inf.u-szeged.hu>
2017-12-03 22:52:48 +0100

    Add SPI interface

--- main.ino
+++ main.ino
@@ -1,15 +1,16 @@
-#include <EEPROM.h>
 #include <SoftwareSerial.h>
+#include <SPIFlash.h>
+#include <SPI.h>
 #include "main.h"
 #include "dht.h"
 
 #define PHOTORESIS_PIN 1
 #define DHT11_PIN 4
-#define LED_PIN 13
 #define RX_PIN 8
 #define TX_PIN 7
 
 SoftwareSerial BLESerial(TX_PIN, RX_PIN); // TX, RX
+SPIFlash flash;
 dht DHT;
 
 int phoVal;
@@ -17,29 +18,21 @@ int i_data = 0;
 int tmp_address;
 int read_tmp = 0;
 int dht_sensor;
+unsigned long past_time;
 
 // BLE communication
 char recieve_buff;
 
-// EEPROM variables
-byte high;
-byte low;
-byte readEEPROM;
+// EEPROM
+uint32_t strAddr = 0;
+char output = "";
 
 void setup() {
   Serial.begin(9600);
   BLESerial.begin(9600);
+  flash.begin();
 
   pinMode(PHOTORESIS_PIN, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
-
-  // Write a 0 to all 1024 bytes of the EEPROM
-  // 1024 bytes in the ATmega328 EEPROM
-  for (int z = 0; z < 1024; z++) {
-    EEPROM.write(z, 0);
-    digitalWrite(13, HIGH); // Turn led on until formating the memory
-  }
-  digitalWrite(13, LOW);
 }
 
 void loop() {
@@ -68,35 +61,35 @@ void loop() {
       case 'c': // c 99
         BLESerial.println("Light module configuration");
         BLESerial.print("ON limit: ");
-        BLESerial.println(EEPROMReadInt(0));
+        //BLESerial.println(EEPROMReadInt(0));
         BLESerial.print("OFF limit: ");
-        BLESerial.println(EEPROMReadInt(2));
+        //BLESerial.println(EEPROMReadInt(2));
         break;
       case 'o': // o 111
         phoVal = analogRead(PHOTORESIS_PIN);
-        EEPROMWriteInt(0, phoVal);
+        //EEPROMWriteInt(0, phoVal);
         BLESerial.print("Light ON config saved: ");
         BLESerial.println(phoVal);
         break;
       case 'q': // q 113
         phoVal = analogRead(PHOTORESIS_PIN);
-        EEPROMWriteInt(2, phoVal);
+        //EEPROMWriteInt(2, phoVal);
         BLESerial.print("Light OFF config saved: ");
         BLESerial.println(phoVal);
         break;
       case 'r': // r 114
         Serial.println("Read the memory data:");
         while (read_tmp != 1023) {
-          readEEPROM = EEPROM.read(read_tmp);
+          //readEEPROM = EEPROM.read(read_tmp);
           Serial.print(read_tmp);
           Serial.print(". byte is ");
-          Serial.println(readEEPROM);
+          //Serial.println(readEEPROM);
           digitalWrite(13, HIGH); // Turn led on until formating the memory
           read_tmp++;
         }
         digitalWrite(13, LOW);
         break;
-      case 's': // s 115
+      /*case 's': // s 115
         BLESerial.print("Start the sampling of the photoresistor values");
         BLESerial.print("Use @ to stop it");
         tmp_address = 4;
@@ -104,7 +97,7 @@ void loop() {
         while (BLESerial.read() != '@') {
           delay(1500);
           phoVal = analogRead(PHOTORESIS_PIN);
-          if (phoVal > (EEPROMReadInt(0) + 100)) {
+         if (phoVal > (EEPROMReadInt(0) + 100)) {
             if (STATUS == 1) {
               BLESerial.println("The lamp is turn off.");
             }
@@ -123,7 +116,7 @@ void loop() {
           }
         }
         BLESerial.println("Stoped the sampling.");
-        break;
+        break;*/
       case 'w':
         dht_sensor = DHT.read11(DHT11_PIN);
         BLESerial.print("Temperature: ");
@@ -134,27 +127,38 @@ void loop() {
         BLESerial.print("Humidity: ");
         BLESerial.println(DHT.humidity);
         break;
+      case 'k':
+        Serial.println("Start sampling temperature & humidity.");
+        Serial.println("Stop the sampling with the /s/ command");
+        char sampling_stopper;
+        while ( sampling_stopper != 's'){
+          if (BLESerial.available()) {
+            sampling_stopper = BLESerial.read();
+          }
+          dht_sensor = DHT.read11(DHT11_PIN);
+          BLESerial.print("Temperature: ");
+          BLESerial.println(DHT.temperature);
+          BLESerial.print("Humidity: ");
+          BLESerial.println(DHT.humidity);
+          past_time = millis(); // get the elapsed time from the uno start
+          flash.writeULong(strAddr, past_time);
+          strAddr += 4;
+          flash.writeShort(strAddr, DHT.humidity);
+          strAddr += 2;
+          flash.writeShort(strAddr, DHT.temperature);
+          strAddr += 2;
+          delay(2000); // DHT11 sensor specific
+        }
+        if (flash.readAnything(0, output)) {
+          Serial.print(F("Read string: "));
+          Serial.println(output);
+          Serial.print(F("From address: "));
+          Serial.println(strAddr);
+        }
+        break;
       default:
         BLESerial.println("Command not found. Check out the available commands with 'h'.");
         break;
     }
   }
-}
-
-
-void EEPROMWriteInt(int address, int value)
-{
-  byte two = (value & 0xFF);
-  byte one = ((value >> 8) & 0xFF);
-
-  EEPROM.update(address, two);
-  EEPROM.update(address + 1, one);
-}
-
-int EEPROMReadInt(int address)
-{
-  long two = EEPROM.read(address);
-  long one = EEPROM.read(address + 1);
-
-  return ((two << 0) & 0xFFFFFF) + ((one << 8) & 0xFFFFFFFF);
 }
